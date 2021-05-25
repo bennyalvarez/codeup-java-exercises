2ebbab0b4e9725dccd28e82dc7d6f351193afc6d
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-05-25 09:48:20 -0500

    completed console exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -2,35 +2,52 @@ import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
+        //1. format floating point numbers
         double pi = 3.14159;
-        System.out.format("The value of pi is approximately %.2f.%n", pi);
+        System.out.format("The value of pi is approximately %.2f%n", pi);
 
-        Scanner scanner = new Scanner(System.in);
-
+        //2. Scanner
         System.out.println("Give me a number.");
+        Scanner scanner = new Scanner(System.in);
         int userNum = scanner.nextInt();
-        System.out.printf("The number you entered is " + userNum + "%n");
+        System.out.printf("The number you entered is %d%n", userNum);
 
+        //3.
         System.out.println("Enter three words.");
         String first = scanner.next();
         String second = scanner.next();
         String third = scanner.next();
         System.out.printf(first + "%n" + second + "%n" + third + "%n");
 
-        scanner.nextLine();
+        //david's version...
+
+        System.out.println("Give me a word!");
+        String str1 = scanner.next();
+        System.out.println("Another one.");
+        String str2 = scanner.next();
+        System.out.println("Another one.");
+        String str3 = scanner.next();
+
+        System.out.printf("You entered the strings: \"%s\", \"%s\", and \"%s\".", str1, str2, str3);
+
+
+
         System.out.print("Please enter a sentence.");
+        scanner.nextLine(); //to clear out the scanner.
         String sentence = scanner.nextLine();
         System.out.print("You have entered: " +sentence);
 
 
+        //Perimeter Calculator
 
-        System.out.println("Please enter values of length and width of a classroom at Codeup.");
-        String length = scanner.nextLine();
-        int valueL = Integer.parseInt(length);
-        String width = scanner.nextLine();
-        int valueH = Integer.parseInt(width);
+        System.out.println("Give me a length in meters:");
+        int length = Integer.parseInt(scanner.nextLine());
+        System.out.printf("Length entered: %d meters. %nGive me a width in meters:%n", length);
+        int width = Integer.parseInt(scanner.nextLine());
+        int area = length * width;
+        int perimeter = (2 * width) + (2 * length);
+        System.out.printf("Length entered: %d meters.%nWidth entered: %d meters. %nArea of room: %d meters squared.%nPerimeter of room: %d meters.%n", length, width, area, perimeter);
 
-        System.out.println("The area of a rectangle is equal to " + valueH * valueL +", and the perimeter of a rectangle is equal to " +valueL*2 + valueH*2 + ".");
 
     }
 }
