3050e85460874221a9dbe82498fa4cd64406a0c7
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-05-27 09:33:50 -0500

    completed BOB exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,13 +1,30 @@
+
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String sure = "Sure?";
-        System.out.println("Who is your favorite uncle?");
-        String maybeBob = scanner.nextLine();
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hey, my name's Bob... My mom said you wanted to talk to me, or something... Whatever...");
+        String response = "";
+        while(!response.equalsIgnoreCase("QUIT")) {
+            System.out.println("What do you want?");
+            response = sc.nextLine();
+            if (response.isEmpty()) {
+                System.out.println("Fine, be that way!");
+            } else if (response.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (response.endsWith("!")) {
+                System.out.println("Woah, chill out!");
+            } else if (response.equalsIgnoreCase("quit")){
+                System.out.println("Finally...");
+            } else {
+                System.out.println("Whatever.");
+            }
+        }
+        System.out.println("I'm out of here...");
     }
 }
+
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,13 +1,15 @@
 public class StringExercise {
     public static void main(String[] args) {
-        String pfloyd1 = "We don't need no education";
+        String pfloyd1 = "We don't need no education\nWe don't need no thought control.";
         System.out.println(pfloyd1);
-        String pfloyd2 = "We don't need no thought control";
-        System.out.println(pfloyd2);
-        String inNout = "Check 'this' out!, 's inside of 's!";
+        // "\n" is new line
+        System.out.println("//////////");
+        String inNout = "Check \"this\" out!, \"s inside of \"s";
         System.out.println(inNout);
+        System.out.println("//////////");
         String windows = "In windows, the main drive is usually C:\\";
         System.out.println(windows);
+        System.out.println("//////////");
         String backSlash = "I can do backslashes \\, double backslashes \\\\,\n" +
                 "and the amazing triple backslash \\\\\\!";
         System.out.println(backSlash);
