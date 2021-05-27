eeeecee3c105f3de2c0058e557498a3503c5f92c
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-05-27 09:06:40 -0500

    starting bob exercise

---
+++ src/Bob.java
@@ -0,0 +1,13 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String sure = "Sure?";
+        System.out.println("Who is your favorite uncle?");
+        String maybeBob = scanner.nextLine();
+
+    }
+}
