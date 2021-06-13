c9856e7f81c9cffe5049bff0b016822db6b63f03
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-06-12 22:51:52 -0500

    more java exercises on if/else and loops

--- src/practice/LearnJava.java
+++ src/practice/LearnJava.java
@@ -16,8 +16,86 @@ public class LearnJava {
 //        System.out.println(sum2);
 //        System.out.println(sum3);
 
+        //if/else/else if statements
 
-        boolean a = 5 > 7;
-        System.out.println(a);
+//        int time = 19;
+//
+//        if(time < 10) {
+//            System.out.println("Good morning");
+//        } else if(time < 20){
+//            System.out.println("Good day");
+//        } else {
+//            System.out.println("Good evening");
+//        }
+
+//        String text = "adobo";
+//
+//        if(text.equals("tacos")) {
+//            System.out.println("Yay! Tacos!");
+//
+//        } else if(text.equals("pizza")) {
+//            System.out.println("Yay! Pizza!");
+//        } else if(text.equals("bulgogi")) {
+//            System.out.println("Yay! Buglogi!");
+//        } else if(text.equals("rice")) {
+//            System.out.println("Yay! Rice");
+//        } else if(text.equals("adobo")) {
+//            System.out.println("Yay! Adobo!");
+//        } else {
+//            System.out.println("Nice Something new!");
+//        }
+            //switch cases are not often used because of some logical issues but good for readability.
+//            int day = 2;
+//
+//            switch(day) {
+//                case 1:
+//                    System.out.println("Monday");
+//                    break;
+//                case 2:
+//                    System.out.println("Tuesday");
+//                    break;
+//                case 3:
+//                    System.out.println("Wednesday");
+//                    break;
+//                case 4:
+//                    System.out.println("Thursday");
+//                    break;
+//                case 5:
+//                    System.out.println("Friday");
+//                    break;
+//                case 6:
+//                    System.out.println("Saturday");
+//                    break;
+//                case 7:
+//                    System.out.println("Sunday");
+//                    break;
+//                default:
+//                    System.out.println("Not a valid day!");
+//            }
+                    //do a do while loop when you don't know how many times to run your code.
+//                    int a = 7;
+//                    do {
+//                        System.out.println("I luv bacon");
+//                        a++;
+//                    } while(a <5);
+//                int count = 1;
+//
+//                while(count <= 10) {
+//                    System.out.println(count);
+//                    count +=1; //the same as count++
+//                }
+                  //if you know how many times you do need to run code...
+//                  String[] cars = {"bmw", "vw", "toyota"};
+//                    //enhanced loop which is a cleaner way.
+//                  for(String c : cars) {
+//                      System.out.println(c);
+//                  }
+                    String s = "Daft Punk";
+
+                    for(int i = 0; i < s.length(); i++) {
+                        System.out.println(s.charAt(i));
+                    }
+
+
     }
 }
