bd0cec920c1207dd860ab8a6674b4015d85907ca
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-07-13 23:56:41 -0500

    java practice

---
+++ src/practice/NapoleonDynamite.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class NapoleonDynamite {
+    String name = "My name is Napoleon Dynamite";
+    long age = 45;
+
+    static void shutUp() {
+        System.out.println("Napolean says 'shuuut uuup!'");
+    }
+}
